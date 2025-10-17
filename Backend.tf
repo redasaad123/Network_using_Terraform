8fa93e683d9ad716d48069486b3a09c3eb944bfa
Reda Saad <Reda.Saad@gmail.com>
2025-10-17 03:23:50 +0300

    commit

---
+++ Backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "Network-group"
+        storage_account_name  = "storage_tfstate"
+        container_name        = "container_tfstate"
+        key                   = "terraform.tfstate"
+    }
+
+}
--- storage.tf
+++ storage.tf
@@ -20,18 +20,7 @@ container_access_type = "private"
 }
 
 
-terraform {
-    backend "azurerm" {
-        resource_group_name  = azurerm_resource_group.name
-        storage_account_name  = azurerm_storage_account.storage_project.name
-        container_name        = azurerm_storage_container.container_project.name
-        key                   = "terraform.tfstate"
-    }
 
-    
-
-    
-}
 
 
 
