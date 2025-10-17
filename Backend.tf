c5627da61814065dfca5a17ef4770e7a94bfba60
Reda Saad <Reda.Saad@gmail.com>
2025-10-17 03:30:57 +0300

    commit

--- Backend.tf
+++ Backend.tf
@@ -1,16 +1,16 @@
 
-resource "azurerm_resource_group" "NetGroup" {
-  name     = "Network-group"
-  location = "West Europe"
-}
+# resource "azurerm_resource_group" "NetGroup" {
+# name     = "Network-group"
+# location = "West Europe"
+# }
 
 
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "Network-group"
-        storage_account_name  = "storage_tfstate"
-        container_name        = "container_tfstate"
-        key                   = "terraform.tfstate"
-    }
+# terraform {
+#     backend "azurerm" {
+#         resource_group_name  = "Network-group"
+#         storage_account_name  = "storage_tfstate"
+#         container_name        = "container_tfstate"
+#         key                   = "terraform.tfstate"
+#     }
 
-}
+# }
--- storage.tf
+++ storage.tf
@@ -1,23 +1,23 @@
 
-# resource "azurerm_resource_group" "NetGroup" {
-# name     = "Network-group"
-# location = "West Europe"
+# # resource "azurerm_resource_group" "NetGroup" {
+# # name     = "Network-group"
+# # location = "West Europe"
+# # }
+
+
+# resource "azurerm_storage_account" "storage_project" {
+# name                     = "storage_tfstate"
+# resource_group_name      = azurerm_resource_group.name
+# location                 = azurerm_resource_group.location
+# account_tier             = "Standard"
+# account_replication_type = "LRS"
 # }
 
-
-resource "azurerm_storage_account" "storage_project" {
-name                     = "storage_tfstate"
-resource_group_name      = azurerm_resource_group.name
-location                 = azurerm_resource_group.location
-account_tier             = "Standard"
-account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container_project" {
-name                  = "container_tfstate"
-storage_account_name  = azurerm_storage_account.storage_project.name
-container_access_type = "private"
-}
+# resource "azurerm_storage_container" "container_project" {
+# name                  = "container_tfstate"
+# storage_account_name  = azurerm_storage_account.storage_project.name
+# container_access_type = "private"
+# }
 
 
 
